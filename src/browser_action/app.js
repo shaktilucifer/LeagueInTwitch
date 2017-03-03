5b4c57bc371e8706e9edbfa59019f7aa8baaba8c
shakti lucifer <shakticse@gmail.com>
2017-03-02 21:28:35 -0500

    add twitch api url

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -1,4 +1,14 @@
 var app = angular.module('twitchApp', []);
-app.controller('twitchController', function($scope) {
+app.controller('twitchController', function($scope,$http) {
     $scope.testing = "test"
+    $http({
+      method: 'GET',
+      url: 'https://api.twitch.tv/kraken/search/streams?q=league%20of%20legends&limit=30'
+    }).then(function successCallback(response) {
+        console.log(response);
+      }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+      });
+
 });
