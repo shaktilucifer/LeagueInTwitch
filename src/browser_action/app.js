2d16486d15f4f6bffbb954a2734b3baa434b19ab
shakti lucifer <shakticse@gmail.com>
2017-03-03 23:55:56 -0500

    Parse json and print all stream names

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -1,15 +1,19 @@
 var app = angular.module('twitchApp', []);
 app.controller('twitchController', function($scope,$http) {
-    $scope.testing = "test"
     $http({
       method: 'GET',
       url: 'https://api.twitch.tv/kraken/streams?q=league%20of%20legends&limit=30',
       headers: {
-        'Client-ID': ''
+        'Client-ID': 'my6s8wrko20vyym17bpy74xkavg6m3'
       }
     }).then(function successCallback(response) {
-        console.log(response);
-
+        var streams = response.data.streams;
+        var streamNames = [];
+        for(var i=0;i < 30 ; i++){
+          streamNames.push(streams[i].channel.display_name);
+        }
+console.log(streamNames);
+$scope.streamNames = streamNames;
       }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
