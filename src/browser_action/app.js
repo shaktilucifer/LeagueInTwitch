592108ed90b13f7da816b2f2cf06bf1a582ba27a
shakti lucifer <shakticse@gmail.com>
2017-03-04 00:14:55 -0500

    Add Stream Names and Stream status

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -2,18 +2,25 @@ var app = angular.module('twitchApp', []);
 app.controller('twitchController', function($scope, $http) {
     $http({
         method: 'GET',
-        url: 'https://api.twitch.tv/kraken/streams?q=league%20of%20legends&limit=30',
+        url: 'https://api.twitch.tv/kraken/streams',
+        params: {
+            game: "league of legends",
+            limit: 19
+        },
         headers: {
             'Client-ID': ''
         }
     }).then(function successCallback(response) {
         var streams = response.data.streams;
         var streamNames = [];
-        for (var i = 0; i < 30; i++) {
-            streamNames.push(streams[i].channel.display_name);
-        }
+        var streamStatus = [];
+        angular.forEach(streams, function(value, key) {
+            streamNames.push(value.channel.display_name);
+            streamStatus.push(value.channel.status);
+        });
         console.log(streamNames);
         $scope.streamNames = streamNames;
+        $scope.streamStatus = streamStatus;
     }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
