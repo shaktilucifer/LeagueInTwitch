2a45f65a884c0ea7697c61826fb7459e7b233b70
shakti lucifer <shakticse@gmail.com>
2017-03-03 23:36:56 -0500

    add clientId and get json

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -3,9 +3,13 @@ app.controller('twitchController', function($scope,$http) {
     $scope.testing = "test"
     $http({
       method: 'GET',
-      url: 'https://api.twitch.tv/kraken/search/streams?q=league%20of%20legends&limit=30'
+      url: 'https://api.twitch.tv/kraken/streams?q=league%20of%20legends&limit=30',
+      headers: {
+        'Client-ID': ''
+      }
     }).then(function successCallback(response) {
         console.log(response);
+
       }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
