d83c2461657919ed666c7bd7f1240c3568d2226d
shakti lucifer <shakticse@gmail.com>
2017-03-03 23:59:15 -0500

    beautify and lint

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -1,22 +1,22 @@
 var app = angular.module('twitchApp', []);
-app.controller('twitchController', function($scope,$http) {
+app.controller('twitchController', function($scope, $http) {
     $http({
-      method: 'GET',
-      url: 'https://api.twitch.tv/kraken/streams?q=league%20of%20legends&limit=30',
-      headers: {
-        'Client-ID': ''
-      }
+        method: 'GET',
+        url: 'https://api.twitch.tv/kraken/streams?q=league%20of%20legends&limit=30',
+        headers: {
+            'Client-ID': ''
+        }
     }).then(function successCallback(response) {
         var streams = response.data.streams;
         var streamNames = [];
-        for(var i=0;i < 30 ; i++){
-          streamNames.push(streams[i].channel.display_name);
+        for (var i = 0; i < 30; i++) {
+            streamNames.push(streams[i].channel.display_name);
         }
-console.log(streamNames);
-$scope.streamNames = streamNames;
-      }, function errorCallback(response) {
+        console.log(streamNames);
+        $scope.streamNames = streamNames;
+    }, function errorCallback(response) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
-      });
+    });
 
 });
